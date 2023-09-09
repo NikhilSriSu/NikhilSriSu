883be805f3b880bee8bd679fcfb27de69218ed5a
NikhilSriSu <nikhilsridhar37@gmail.com>
2023-09-09 12:49:00 +0530

    new push

---
+++ src/Content.js
@@ -0,0 +1,180 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <>
+        <div style={{position:"relative",zIndex:0, marginTop:"-139pc", height:"100pc"}}>
+         <b style={{color:"white", marginLeft:"6pc"}}>Login</b>
+         <b style={{color:"white", marginLeft:"42pc"}}>About</b>
+         <b style={{color:"white", marginLeft:"6pc"}}>Pricing</b>
+         <b style={{color:"white", marginLeft:"6pc"}}>Review</b>
+
+       <div style={{marginLeft:"6pc", marginTop:"7pc"}}>
+       <h1 style={{color:"white", marginTop:"60px", marginBottom:"-15px"}}>Learn how to launch a</h1>
+       <h1 style={{color:"white"}}>successful podcast</h1>
+       <p style={{color:"white"}}>Lorem Ipsum is simply dummy text of the printing and </p>
+       <p style={{color:"white"}}>typesetting in ustry. Lorem Ipsum has been the</p>
+       <p style={{color:"white"}}>industry's standard dummy text ever since the 1500s</p>
+
+       <button className='button'>Sign up now</button>
+ 
+       <div className="dot" style={{marginTop:"-16pc"}}></div>
+      <div className="dot">  <img className="image" src="Vector.png" alt="Image 1" style={{marginTop:"80px"}}/></div>
+      <div className="dot" style={{marginTop:"-10pc"}}></div>
+      <div className="dot" style={{marginTop:"-7pc"}}></div>
+      <div className="dot" style={{marginTop:"-4pc"}}><img className="image" src="Group.png" alt="Image 1" style={{marginLeft:"-85px", marginTop:"33px"}}/></div>
+      <div className="dot" style={{marginTop:"-1pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc"}}></div>
+      <div className="dot" style={{marginTop:"-4pc", marginLeft:"65pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"65pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"60pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"55pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"50pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"45pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"40pc"}}></div>
+      <di3 className="dot" style={{marginTop:"2pc", marginLeft:"35pc"}}></di3>
+       </div>
+       <div className="image-container" style={{marginTop:"-22pc", marginLeft:"43pc"}}>
+      <img className="image" src="everyone-is-smiling-listens-group-people-business-conference-modern-classroom-daytime 1.png" alt="Image 1" />
+      <img className="image" src="group-business-workers-smiling-happy-confident-working-together-with-smile-face-applauding-one-them-office 1.png" alt="Image 2" style={{marginTop:"75px", marginLeft:"-100px"}}/>
+    </div>
+    <div style={{border:"solid 1px #7A3199", borderRadius:"10px" ,padding:"10px", marginLeft:"6pc", marginTop:"35pc", width:"15pc"}}>
+         <h2 style={{color:"black"}}>Interactive Features</h2>
+         <p>Lorem Ipsum is simply dummy  text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+        <div style={{border:"solid 1px #7A3199", borderRadius:"10px" ,padding:"10px", marginLeft:"24pc", marginTop:"-8pc", width:"15pc"}}>
+         <h2 style={{color:"black"}}>Interactive Features</h2>
+         <p>Lorem Ipsum is simply dummy  text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+        <div style={{border:"solid 1px #7A3199", borderRadius:"10px" ,padding:"10px", marginLeft:"6pc", marginTop:"0pc", width:"15pc"}}>
+         <h2 style={{color:"black"}}>Interactive Features</h2>
+         <p>Lorem Ipsum is simply dummy  text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+        <div style={{border:"solid 1px #7A3199", borderRadius:"10px" ,padding:"10px", marginLeft:"24pc", marginTop:"-8pc", width:"15pc"}}>
+         <h2 style={{color:"black"}}>Interactive Features</h2>
+         <p>Lorem Ipsum is simply dummy  text of the printing and typesetting industry. Lorem Ipsum has been</p>
+        </div>
+        <div style={{ marginTop: "-24pc", marginLeft: "50pc"}}>
+            <h1> About the Course </h1>
+            <p style={{width:"18pc"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unkno</p>
+            <button className='button'>Explore now</button>
+        </div>
+
+        <div style={{ marginTop: "13pc", display:"flex", justifyContent:"center"}}>
+            <h1> Choose Your Plan</h1>
+        </div>
+        <div style={{ marginTop: "-2pc", display:"flex", justifyContent:"center"}}>
+            <p style={{width:"33pc"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
+        </div>
+        <div style={{display:"flex", justifyContent:"space-around"}}>
+        <div style={{border:"solid 1px", borderRadius:"10px" ,padding:"10px", paddingLeft:"25px", marginTop:"5pc", width:"15pc"}}>
+         <h2>Basic Plan</h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+         <h1 style={{color:" var(--ghost, #7A3199)"}}>$ 54 <span style={{fontSize:"20px", fontWeight:"300", marginTop:"-10px"}}>/month</span></h1>
+         <ul style={{marginLeft:"-15px"}}>
+            <li>Free access to video class</li>
+            <li>Free access to video class</li>
+            <li>Free access to video class</li>
+        </ul>
+        <button style={{border:"none", height: "50px", width: "156px", borderRadius:"10px", borderColor:"#7A3199", color:"#7A3199", fontSize:"18px"}}>Start Free Trail</button>
+        </div>
+        <div className='cardShadow' style={{border:"solid 1px", borderRadius:"10px" ,padding:"10px", paddingLeft:"25px", marginTop:"5pc", width:"15pc", background:"#7A3199", color:"white"}}>
+         <h2 style={{color:"White"}}>Premium Plan</h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+         <h1 style={{color:"white"}}>$ 54 <span style={{fontSize:"20px", fontWeight:"300", marginTop:"-10px"}}>/month</span></h1>
+         <ul style={{marginLeft:"-15px"}}>
+            <li>Free access to video class</li>
+            <li>Free access to video class</li>
+            <li>Free access to video class</li>
+        </ul>
+        <button style={{border:"none", height: "50px", width: "156px", borderRadius:"10px", borderColor:"#7A3199", color:"#7A3199", fontSize:"18px"}}>Start Free Trail</button>
+        </div>
+        <div style={{border:"solid 1px", borderRadius:"10px" ,padding:"10px", paddingLeft:"25px",marginTop:"5pc", width:"15pc"}}>
+         <h2>Basic Plan</h2>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+         <h1 style={{color:" var(--ghost, #7A3199)"}}>$ 54 <span style={{fontSize:"20px", fontWeight:"300", marginTop:"-10px"}}>/month</span></h1>
+         <ul style={{marginLeft:"-15px"}}>
+            <li>Free access to video class</li>
+            <li>Free access to video class</li>
+            <li>Free access to video class</li>
+        </ul>
+        <button style={{border:"none", height: "50px", width: "156px", borderRadius:"10px", borderColor:"#7A3199", color:"#7A3199", fontSize:"18px"}}>Start Free Trail</button>
+        </div>
+        </div>
+
+        <div style={{marginTop:"5pc", marginLeft:"5pc"}}>
+            <h1>Review from customers</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+        <div style={{display:"flex", justifyContent:"space-around", marginTop:"-3pc", marginLeft:"-5pc", alignItems:"center"}}>
+        <div className='cardShadow' style={{ borderRadius:"10px" ,padding:"10px", paddingLeft:"25px", marginTop:"5pc", width:"24pc", height:"10pc"}}>
+          <div style={{display:"flex"}}>
+          <img src="Lolla1.png" alt="Image 2" />
+         <h2 style={{marginTop:"10px", marginLeft:"20px"}}>Lolla Smith</h2>
+          </div>
+          <p style={{marginLeft:"91px", marginTop:"-28px"}}>Microsoft</p>
+          <div style={{display:"flex", marginLeft: "89px", marginTop: "-10px"}}>
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          </div>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        </div>
+        
+        <div style={{ borderRadius:"10px" ,padding:"10px", paddingLeft:"25px", marginTop:"5pc", width:"24pc", height:"10pc"}}>
+          <div style={{display:"flex"}}>
+          <img src="Lolla1.png" alt="Image 2" />
+         <h2 style={{marginTop:"10px", marginLeft:"20px"}}>Lolla Smith</h2>
+          </div>
+          <p style={{marginLeft:"91px", marginTop:"-28px"}}>Microsoft</p>
+          <div style={{display:"flex", marginLeft: "89px", marginTop: "-10px"}}>
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          <img src="fluent-emoji-flat_star.png" alt="Image 2" />
+          </div>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        </div>
+        <img src="pepicons-pencil_arrow-right.png" alt="Image 2" height={35}/>
+        </div>
+        <div className="dot" style={{marginTop:"2pc", marginLeft:"33pc"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"36pc", background:"grey"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"39pc", background:"grey"}}></div>
+      <div className="dot" style={{marginTop:"2pc", marginLeft:"42pc", background:"grey"}}></div>
+        </div>
+
+        <div style={{ marginTop: "6pc", display:"flex", justifyContent:"center"}}>
+            <h1> We have what you're looking for</h1>
+        </div>
+        <div style={{ marginTop: "-1pc", display:"flex", justifyContent:"center"}}>
+            <p style={{width:"43pc", color:"#4776E6 "}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type a</p>
+        </div>
+
+        <div style={{ display:"flex", justifyContent:"center"}}>
+        <button className='button'><b>Get Started now</b></button>
+        </div>
+
+        <div className='footer' style={{borderTop:"solid 1px", marginTop:"5pc", height:"20px"}}>
+            <span>All Right Reserved @Copyright 2023</span>
+
+           <div className='footer1'>
+           <span>Terms of Service</span>
+            <span>Privacy Policy</span>
+            <span>Product</span>
+           </div>
+           <div className='footer2'>
+           <img src="mdi_facebook.png" alt="Image 2" />
+           <img src="uil_youtube.png" alt="Image 2" />
+           <img src="basil_instagram-solid.png" alt="Image 2" />
+           <img src="uil_twitter.png" alt="Image 2" />
+           </div>
+        </div>
+  </div>
+ 
+  </>
+  )
+}
+
+export default Content
---
+++ src/New.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default New
